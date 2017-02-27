dda587af64cca76bfb366cc6a17692f0a598aa61
adam-ali <adam_ali_96@hotmail.co.uk>
2017-02-27 10:58:17 +0000

    set up directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+    imports: [
     BrowserModule,
     FormsModule,
     HttpModule
+  ],
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    ListComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
